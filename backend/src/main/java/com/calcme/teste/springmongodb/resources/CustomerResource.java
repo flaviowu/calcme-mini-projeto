345cf36683bc54b85829a35886dfde1507e8aa8b
Flavio Wu <flaviowu@gmail.com>
2022-01-16 17:00:06 -0300

    adicionado configurção de cors policy

--- backend/src/main/java/com/calcme/teste/springmongodb/resources/CustomerResource.java
+++ backend/src/main/java/com/calcme/teste/springmongodb/resources/CustomerResource.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping(value="/customers")
 public class CustomerResource {
 
