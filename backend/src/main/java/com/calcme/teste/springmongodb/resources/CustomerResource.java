476054ca001ffe0ddadcb0b939e50ad82ebd887f
Flavio Wu <flaviowu@gmail.com>
2022-01-15 22:03:08 -0300

    inserção de usuário com POST

--- backend/src/main/java/com/calcme/teste/springmongodb/resources/CustomerResource.java
+++ backend/src/main/java/com/calcme/teste/springmongodb/resources/CustomerResource.java
@@ -5,11 +5,11 @@ import com.calcme.teste.springmongodb.dto.CustomerDTO;
 import com.calcme.teste.springmongodb.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.Servlet;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,5 +31,13 @@ public class CustomerResource {
     public ResponseEntity<CustomerDTO> findById(@PathVariable String id) {
         Customer obj = service.findById(id);
         return ResponseEntity.ok().body(new CustomerDTO(obj));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody CustomerDTO objDto) {
+        Customer obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- backend/src/main/java/com/calcme/teste/springmongodb/services/CustomerService.java
+++ backend/src/main/java/com/calcme/teste/springmongodb/services/CustomerService.java
@@ -1,6 +1,7 @@
 package com.calcme.teste.springmongodb.services;
 
 import com.calcme.teste.springmongodb.domain.Customer;
+import com.calcme.teste.springmongodb.dto.CustomerDTO;
 import com.calcme.teste.springmongodb.repository.CustomerRepository;
 import com.calcme.teste.springmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,14 @@ public class CustomerService {
     public Customer findById(String id) {
         Optional<Customer> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não Encontrado"));
+    }
+
+    public Customer insert(Customer obj) {
+        return repo.insert(obj);
+    }
+
+    public Customer fromDTO(CustomerDTO objDto) {
+        return new Customer(objDto.getId(), objDto.getName(), objDto.getPhoneNumber(), objDto.getEmail() );
     }
 
 }
