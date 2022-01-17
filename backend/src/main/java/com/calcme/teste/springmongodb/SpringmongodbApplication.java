811a8b5637011ac841916b3111acf027bcc45e58
Flavio Wu <flaviowu@gmail.com>
2022-01-17 00:01:40 -0300

    ajustes para gerar o .war

--- backend/src/main/java/com/calcme/teste/springmongodb/SpringmongodbApplication.java
+++ backend/src/main/java/com/calcme/teste/springmongodb/SpringmongodbApplication.java
@@ -2,12 +2,19 @@ package com.calcme.teste.springmongodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringmongodbApplication {
+public class SpringmongodbApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmongodbApplication.class, args);
+	}
+
+		@Override
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+			return application.sources(SpringmongodbApplication.class);
 	}
 
 }
